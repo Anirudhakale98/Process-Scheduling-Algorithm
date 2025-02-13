85fab6a4241ffbb1e082ceec80d5cc26d80d4ad1
Anirudhakale98 <anirudhakale982354@gmail.com>
2025-02-13 13:01:27 +0530

    PriorityScheduling algorithm

--- PriorityScheduling.java
+++ PriorityScheduling.java
@@ -9,71 +9,68 @@ public class PriorityScheduling {
     static Scanner sc = new Scanner(System.in);
 
     public PriorityScheduling(Process[] p) {
-
         System.out.println("Enter Priority of given process: (0-has highest priority)");
         for (int i = 0; i < p.length; i++) {
             System.out.print(p[i].process_id + ": ");
             p[i].priority = sc.nextInt();
-            System.out.println();
         }
 
         System.out.println("Priority Scheduling Algorithm of given process is:");
 
         priorityScheduling(p);
-        // printProcess(p);
+
         Output op = new Output();
         op.printProcess(p);
         op.printAvgTAT(p);
         op.printAvgWT(p);
-
     }
 
+    // Sorting processes by arrival time only
     public static class sortByAt implements Comparator<Process> {
-
         public int compare(Process p1, Process p2) {
-            // if (p1.arrivalTime == p2.arrivalTime) {
-            // return Integer.compare(p2.priority, p1.priority);
-            // }
             return Integer.compare(p1.arrivalTime, p2.arrivalTime);
-
         }
-
     }
 
     public static void priorityScheduling(Process p[]) {
-        Arrays.sort(p, new sortByAt());
+        Arrays.sort(p, new sortByAt()); // Sort by arrival time
         PriorityQueue<Process> pq = new PriorityQueue<>(new Comparator<Process>() {
             public int compare(Process p1, Process p2) {
                 if (p1.priority == p2.priority) {
                     return Integer.compare(p1.arrivalTime, p2.arrivalTime);
                 }
-                return Integer.compare(p1.priority, p2.priority);
+                return Integer.compare(p1.priority, p2.priority); // Lower value = higher priority
             }
         });
 
-        int time = 0;
-        int i = 0;
+        int time = 0; // Track CPU time
+        int i = 0; // Index for process array
         int n = p.length;
+
         while (!pq.isEmpty() || i < n) {
+            // Add all processes that have arrived until the current time
             while (i < n && p[i].arrivalTime <= time) {
                 pq.add(p[i]);
                 i++;
             }
+
             if (!pq.isEmpty()) {
+                // Get highest-priority process
                 Process curr = pq.poll();
-                time = time + curr.burstTime;
+                time += curr.burstTime;
                 curr.completionTime = time;
                 curr.calculateTAT();
                 curr.calculateWT();
+            } else {
+                // If no process is available, move to the next arrival time
+                if (i < n) {
+                    time = p[i].arrivalTime;
+                }
             }
         }
-
     }
 
     public static void main(String[] args) {
-
-        // System.out.println("Scheduling Algorithm");
-
+        // Test your code here if needed
     }
-
 }
--- testing.java
+++ testing.java
@@ -2,14 +2,19 @@ package SchedulingAlgorithms.test;
 
 import SchedulingAlgorithms.Process;
 
+import java.util.PriorityQueue;
+
 import SchedulingAlgorithms.FCFS;
 import SchedulingAlgorithms.Input;
 import SchedulingAlgorithms.Output;
+import SchedulingAlgorithms.PriorityScheduling;
 
 public class testing {
 
     public static void main(String[] args) {
         System.out.println("Scheduling Algorithm");
+        Process p[] = { new Process("p1", 2, 3), new Process("p2", 3, 4), new Process("p3", 4, 5) };
 
+        new PriorityScheduling(p);
     }
 }
